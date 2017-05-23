2212b1605e318268bcd2cca64d0735f5bb2e569b
Wing Gao <wing.gao@live.com>
2017-05-23 16:16:29 +0800

    添加fetch

--- js/comm/req.js
+++ js/comm/req.js
@@ -1,6 +1,4 @@
-/**
- * Created by gaoyunyun on 2017/5/17.
- */
+import 'whatwg-fetch'
 
 function regJqPostJSON() {
     $.postJSON = function (url, data, callback) {
@@ -18,8 +16,38 @@ function regJqPostJSON() {
     };
 }
 
+function fetchJSON(url, opts) {
+    return fetch(url, opts).then((response) => {
+        if (response.ok) {
+            return response.json()
+        } else {
+            let error = new Error(response.statusText)
+            error.response = response
+            throw error
+        }
+    })
+}
+
+function fPostJSON(url, data, opts = {}) {
+    return fetchJSON(url, _.merge({
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+        body: params(data)
+    }, opts))
+}
+
+function params(obj) {
+    let res = []
+    _.forEach(obj, (v, k) => {
+        res.push(`${k}=${encodeURIComponent(v)}`)
+    })
+    return res.join('&')
+}
+
 export {
-    regJqPostJSON,
+    regJqPostJSON, params, fetchJSON, fPostJSON,
 }
 
 

c0f44388f2ea840bd43e082e5a641aae939de159
Wing Gao <wing.gao@live.com>
2018-05-16 21:48:14 +0800

    添加 modal

--- js/comm/antd-cn/index.d.ts
+++ js/comm/antd-cn/index.d.ts
@@ -1,13 +1,15 @@
 import { ModalFuncProps, ModalProps, ModalFunc } from 'antd/lib/modal/Modal';
 
 export class ModalCN {
-    static error(props: ModalFuncProps): ModalFunc;
+  static error(props: ModalFuncProps): ModalFunc;
 
-    static success(props: ModalFuncProps): ModalFunc;
+  static success(props: ModalFuncProps): ModalFunc;
 
-    static confirm(props: ModalFuncProps): ModalFunc;
+  static confirm(props: ModalFuncProps): ModalFunc;
 
-    static build(props: ModalFuncProps & { content: any }): ModalFunc;
+  static confirmDelete(props: ModalFuncProps): ModalFunc;
+
+  static build(props: ModalFuncProps & { content: any }): ModalFunc;
 }
 
 export * from './data-picker';
--- js/comm/antd-cn/modal.js
+++ js/comm/antd-cn/modal.js
@@ -22,4 +22,12 @@ export class ModalCN {
         props = merge({ okText: '确定', cancelText: '取消' }, props);
         return <Modal {...props}>{props.content}</Modal>;
     }
+
+    static confirmDelete(props) {
+        props = merge({
+            okText: '删除', okType: 'danger', title: '确认删除', cancelText: '取消',
+            content: '是否删除？',
+        }, props);
+        return Modal.confirm(props);
+    }
 }
